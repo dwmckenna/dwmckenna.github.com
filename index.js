d43cc935e613d6e6f4ba283d5c113afecfedbf25
Patrick <patrick@bittorrent.com>
2012-03-11 15:18:41 -0700

    linking to jquery and adding a wee bit of javascript to our project

---
+++ index.js
@@ -0,0 +1,8 @@
+$(function() {
+	//hey daniel, this is a comment...we write these to remind 
+	//ourselves and others what the code is suppose to do...to
+	//create a commend just start the line with //
+
+	//lets start with something simple.
+	alert('hi');
+});
